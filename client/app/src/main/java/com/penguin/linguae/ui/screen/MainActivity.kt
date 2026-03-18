542db997004858b83dc09cccc3ec86d848b15b94
Yunetrea <153362833+HaoKiet10@users.noreply.github.com>
2026-03-18 16:00:57 +0700

    Move MainActivity to ui.screen package
    Relocated MainActivity into the com.penguin.linguae.ui.screen package (moved file to ui/screen/MainActivity.kt) and updated references accordingly. Updated AndroidManifest.xml to reference .ui.screen.MainActivity, adjusted activity_main.xml tools:context to the new path, and added the R import in the activity file.

--- client/app/src/main/java/com/penguin/linguae/ui/screen/MainActivity.kt
+++ client/app/src/main/java/com/penguin/linguae/ui/screen/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.penguin.linguae
+package com.penguin.linguae.ui.screen
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.penguin.linguae.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
