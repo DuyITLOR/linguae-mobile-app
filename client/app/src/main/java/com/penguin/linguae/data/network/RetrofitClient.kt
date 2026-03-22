b344df3968c101ceb3139f0e872ce26a21f2daef
ThanhDang-Vn <ntdang23@clc.fitus.edu.vn>
2026-03-22 09:25:47 +0700

    feat: :sparkles: reorganize project structure and add base architechture

---
+++ client/app/src/main/java/com/penguin/linguae/data/network/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.penguin.linguae.data.network
+
+import com.penguin.linguae.data.network.api.VocabularyApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://10.0.2.2:3306/"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl((BASE_URL))
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val vocabularyApi: VocabularyApi by lazy {
+        retrofit.create(VocabularyApi::class.java)
+    }
+
+//    val columnApi: ColumnApi by lazy {
+//        retrofit.create(ColumnApi::class.java)
+//    }
+//
+//    val userApi: UserApi by lazy {
+//        retrofit.create(UserApi::class.java)
+//    }
+//
+//    val authApi: AuthApi by lazy {
+//        retrofit.create(AuthApi::class.java)
+//    }
+}
