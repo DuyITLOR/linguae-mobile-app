d6edbf2a1ee851a51fc7eb971806d2a2fb2baca1
DuyITLOR <153221271+DuyITLOR@users.noreply.github.com>
2026-03-28 00:17:42 +0700

    feat: ✨ Complete the UI for the forgot password feature

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.foundation)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
