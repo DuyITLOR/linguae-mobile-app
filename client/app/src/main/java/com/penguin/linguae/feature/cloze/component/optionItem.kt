0f6bc96dc0517a402212bec29ada794b80b5f39c
Yunetrea <153362833+HaoKiet10@users.noreply.github.com>
2026-04-12 22:04:53 +0700

    style: change color of cloze option

--- client/app/src/main/java/com/penguin/linguae/feature/cloze/component/optionItem.kt
+++ client/app/src/main/java/com/penguin/linguae/feature/cloze/component/optionItem.kt
@@ -3,7 +3,10 @@ package com.penguin.linguae.feature.cloze.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,8 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.penguin.linguae.core.ui.theme.Black
 import com.penguin.linguae.core.ui.theme.Green
@@ -43,12 +48,52 @@ fun OptionItem(
         modifier = Modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(12.dp))
-            .border(1.dp, TextGray, RoundedCornerShape(12.dp))
-            .background(backgroundColor)
-            .clickable(enabled = isCorrect == null) { onClick() }
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+            .border(2.dp, backgroundColor, RoundedCornerShape(12.dp))
+            .background(SurfaceColor)
+            .clickable(enabled = isCorrect == null) { onClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = text, color = Black, fontWeight = fontWeight)
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(backgroundColor.copy(alpha = 0.2f))
+            )
+            Text(
+                text = text,
+                color = Black,
+                fontWeight = fontWeight,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun test(){
+    Column() {
+        OptionItem(
+            text = "111",
+            isSelected = true,
+            isCorrect = true,
+        )
+        OptionItem(
+            text = "111",
+            isSelected = false,
+            isCorrect = true,
+        )
+        OptionItem(
+            text = "111",
+            isSelected = true,
+            isCorrect = false,
+        )
+        OptionItem(
+            text = "111",
+            isSelected = false,
+            isCorrect = false,
+        )
     }
 }
