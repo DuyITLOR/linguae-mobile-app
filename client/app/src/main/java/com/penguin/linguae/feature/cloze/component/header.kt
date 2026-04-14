69e2e79f3a5d35ec4cb6d4eb3009e68978e23963
Yunetrea <153362833+HaoKiet10@users.noreply.github.com>
2026-04-14 08:55:43 +0700

    Style: Change Cloze Header Color

--- client/app/src/main/java/com/penguin/linguae/feature/cloze/component/header.kt
+++ client/app/src/main/java/com/penguin/linguae/feature/cloze/component/header.kt
@@ -14,13 +14,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.penguin.linguae.R
+import com.penguin.linguae.core.ui.theme.PurpleBlueTheme
 import com.penguin.linguae.core.ui.theme.SurfaceColor
 import com.penguin.linguae.core.ui.theme.TextGray
 import com.penguin.linguae.feature.cloze.viewmodel.ClozeViewModel
@@ -34,35 +37,50 @@ fun Header(
         .fillMaxWidth()
         .background(SurfaceColor)
         .shadow(1.dp)
-        .padding(vertical = 12.dp),
     ){
-        Row(
+        Box(
             modifier = Modifier
-                .fillMaxWidth(0.95f)
-                .align(Alignment.Center),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            IconButton(onClick = onClick) {
-                Icon (
-                    painter = painterResource(R.drawable.return_arrow),
-                    modifier = Modifier.size(36.dp),
-                    contentDescription = null,
-                )
-            }
+                .fillMaxWidth()
+                .background(PurpleBlueTheme)
+                .alpha(0.8f)
+                .padding(vertical = 12.dp),
+            ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .align(Alignment.Center),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                IconButton(onClick = onClick) {
+                    Icon(
+                        painter = painterResource(R.drawable.return_arrow),
+                        modifier = Modifier.size(36.dp),
+                        contentDescription = null,
+                        tint = SurfaceColor,
+                    )
+                }
 
-            Column () {
-                Text(
-                    text = "Điền vào chỗ trống",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
-                )
-                Text(
-                    text = "Câu ${questionNo + 1} / 10",
-                    color = TextGray,
-                    fontSize = 16.sp,
-                )
+                Column() {
+                    Text(
+                        text = "Điền vào chỗ trống",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = SurfaceColor,
+                    )
+                    Text(
+                        text = "Câu ${questionNo + 1} / 10",
+                        color = SurfaceColor,
+                        fontSize = 16.sp,
+                    )
+                }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun Test(){
+    Header(1)
 }
